f4034d3c7d39d3913851e2d2836dd93245463acd
mickmed <mick2090@gmail.com>
2016-06-18 17:49:30 -0400

    a

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,28 +4,23 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [800, 800]
 
- 
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  
   def store_dir
-   
-
-  "$OPENSHIFT_DATA_DIR/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-
-
+   prefix = ENV['OPENSHIFT_DATA_DIR'] ? "#{ENV['OPENSHIFT_DATA_DIR']}/" : ""
+  "#{prefix}uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-
-
-
-
-
-
-  # Add a white list of extensions which are allowed to be uploaded.dddd
-  def extension_white_list
-    %w(jpg jpeg gif png)
+  def url
+   return "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}/#{File.basename(file.path)}" if ENV['OPENSHIFT_DATA_DIR'] && file
+   super
   end
+
+# Add a white list of extensions which are allowed to be uploaded.dddd
+ def extension_white_list
+  %w(jpg jpeg gif png)
+ end
 end
 
 

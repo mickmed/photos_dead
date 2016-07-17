1b4e8d56d6cd7a7afd572cc5391b957305b079cb
mickmed <mick2090@gmail.com>
2016-07-16 20:58:06 -0400

    a

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
- config.assets.enabled = true  
- config.assets.paths << "#{Rails.root}/app/assets"   
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Rails4Example
   class Application < Rails::Application
+     config.assets.enabled = true  
+ config.assets.paths << "#{Rails.root}/app/assets"   
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

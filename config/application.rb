61a35135a42a8f9e8d4bf9103e312a977714ded0
mickmed <mick2090@gmail.com>
2016-07-16 20:53:38 -0400

    a

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
  config.assets.enabled = true  
- config.assets.paths << "#{Rails.root}/app/assets/fonts"   
+ config.assets.paths << "#{Rails.root}/app/assets"   
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)

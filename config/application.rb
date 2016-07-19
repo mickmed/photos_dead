2fdbb877cb1edfa110d4174f9b90dee013f02f5c
mickmed <mick2090@gmail.com>
2016-07-19 00:28:54 -0400

    aa

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Rails4Example
   class Application < Rails::Application
-    #config.assets.enabled = true  
-    #config.assets.paths << Rails.root.join("app", "assets", "fonts")  
+    config.assets.enabled = true  
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

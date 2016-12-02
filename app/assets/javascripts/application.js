ae3aa0ec8d8f9e11482170c275732e57cbd749d8
mickmed <mick2090@gmail.com>
2016-12-01 20:32:31 -0500

    fullscreen fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,30 +18,57 @@
 
 //= require_tree .
 
-    
+ function slider(){ var windowHeight = $(window).height();
+	  var headerHeight = $("header").outerHeight();
+	  var calculatedHeight = windowHeight - headerHeight;
+	  var heightFill = $('.height-fill')
+	  $(heightFill).height(calculatedHeight);
+	  // superslides
+	  $(function()  {
+	    $('#slides').superslides({
+	        inherit_height_from: '.height-fill'
+	        
+	    });
+	  });
+	  }
+	
+
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
 
 
+	document.addEventListener("webkitfullscreenchange", function () {
+	    slider();
+	}, false);
+	document.addEventListener("fullscreenchange", function () {
+	    slider();
+	}, false);
+	document.addEventListener("msfullscreenchange", function () {
+	   slider();
+	}, false);
+	document.addEventListener("mozfullscreenchange", function () {
+	    slider();
+	}, false);
 
-$(document).ready(function() { 
- document.getElementById("fullscreen").onclick = function() {myFunction()};
+}
 
- function myFunction() {
-  document.getElementById('slides').style.display = "block";
-  var windowHeight = $(window).height();
-  var headerHeight = $("header").outerHeight();
-  var calculatedHeight = windowHeight - headerHeight;
-  var heightFill = $('.height-fill')
-  $(heightFill).height(calculatedHeight);
-  // superslides
-  $(function()  {
-    $('#slides').superslides({
-        inherit_height_from: '.height-fill',
-        
-    });
-  });
- };
- 
-});
 
 
     
